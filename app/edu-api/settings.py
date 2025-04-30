0c447e841f5be6aa84b375dff3f03588a9cba79a
GiHoon Jang <stb.jang@gmail.com>
2025-04-30 17:28:09 +0900

    Create settings.py

---
+++ app/edu-api/settings.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'dein-geheimer-schlüssel'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Weitere Apps
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    # Weitere Middleware
+]
+
+ROOT_URLCONF = 'edu_api.urls'
+
+WSGI_APPLICATION = 'edu_api.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
