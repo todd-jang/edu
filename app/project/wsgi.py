6b649bb0e70514214bde7363c91962166b6e5797
GiHoon Jang <stb.jang@gmail.com>
2025-05-01 00:30:29 +0900

    final 00 upto mid-size service| MVPs wsgi.py

--- app/project/wsgi.py
+++ app/project/wsgi.py
@@ -1,4 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+application = WhiteNoise(application)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 application = get_wsgi_application()
