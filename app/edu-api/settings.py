551e2d30b8ede6aaae4101ce353d7a708501b8f1
GiHoon Jang <stb.jang@gmail.com>
2025-05-01 01:48:10 +0900

    Update incl whitenoise cache option settings.py

--- app/edu-api/settings.py
+++ app/edu-api/settings.py
@@ -1,5 +1,10 @@
 from pathlib import Path
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Optional: für zusätzliche Komprimierung & Caching
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'edu_api/static'),
@@ -22,6 +27,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ⬅️ hier
+    
     # Weitere Middleware
 ]
 
