251aea29d06302ff06972c7824fb17b8e9905f9d
GiHoon Jang <stb.jang@gmail.com>
2025-05-01 00:48:46 +0900

    final whitenoise for MVPs wsgi.py

--- app/project/wsgi.py
+++ app/project/wsgi.py
@@ -2,6 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-application = WhiteNoise(application)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 application = get_wsgi_application()
+application = WhiteNoise(application)
