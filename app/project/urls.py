2b25bc0bd69157731945a8cf7f57364962712d5d
GiHoon Jang <stb.jang@gmail.com>
2025-05-01 12:15:38 +0900

    production

--- app/project/urls.py
+++ app/project/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf import settings #nur für Entwicklungsumgebung
+from django.conf.urls.static import static #nur für Entwicklungsumgebung
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('edu.urls')),
 ]
-if settings.DEBUG:
+if settings.DEBUG:#nur für Entwicklungsumgebung
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
